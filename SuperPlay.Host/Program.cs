ad28151abc962099e6ffefbeb9b1e36541b1d82d
Andrey Dyachenko <d0535318380@gmail.com>
2023-10-20 23:07:41 +0300

    Add logger

--- SuperPlay.Host/Program.cs
+++ SuperPlay.Host/Program.cs
@@ -1,7 +1,15 @@
+using Serilog;
+using Serilog.Formatting.Compact;
 using SuperPlay.Abstractions.Services;
+using SuperPlay.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
 
+services.AddHostedService<GameService>();
+services.AddSingleton<IGameService, GameService>(sp => sp.GetRequiredService<GameService>());
+services.AddSerilog(cfg => 
+    cfg.WriteTo.Async(wt => wt.Console(new RenderedCompactJsonFormatter())));
 
 var app = builder.Build();
 
@@ -9,7 +17,6 @@ app.UseWebSockets();
 
 
 app.MapGet("/ws", async (IGameService gameService, HttpContext httpContext, CancellationToken token) =>
-
 {
     if (!httpContext.WebSockets.IsWebSocketRequest)
     {
